6fe97da0b9feb0d6f25c5b069be3e1312aa56cf9
Palak Bansal <palakbansal5002@gmail.com>
2026-02-17 21:30:52 +0530

    Solution to first question

---
+++ q1.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findKilled(vector<int>&pid, vector<int>&ppid, int kill){
+    int n=pid.size();
+    queue<int> q;
+    vector<int>res;
+    q.push(kill);
+    while(!q.empty()){
+        int top=q.front();
+        q.pop();
+        for(int i=0;i<n;i++){
+            if(ppid[i]==top)
+                q.push(pid[i]);
+        }
+        res.push_back(top);
+    }
+    return res;
+}
+
+int main(){
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    vector<int> pid(n);
+    vector<int> ppid(n);
+    int kill;
+    cout<<"enter kill";
+    cin>>kill;
+    cout<<"enter pid: ";
+    for(int i=0;i<n;i++){
+        cin>>pid[i];
+    }
+    cout<<"enter ppid: ";
+    for(int i=0;i<n;i++){
+        cin>>ppid[i];
+    }
+
+    vector<int> res= findKilled(pid,ppid,kill);
+    int m=res.size();
+    for(int i=0;i<m;i++){
+        cout<<res[i]<<" ";
+    }
+}
